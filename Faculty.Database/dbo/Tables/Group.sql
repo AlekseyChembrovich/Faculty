c51a1bd41c1ebb631d0e924f96cef79b383b94f0
AlekseyGit-LegionY530 <chembrovich0303@mail.ru>
2021-08-31 12:01:20 +0300

    Create  foreign key SpecialozationId (group)

--- Faculty.Database/dbo/Tables/Group.sql
+++ Faculty.Database/dbo/Tables/Group.sql
@@ -1,5 +1,7 @@
 ﻿CREATE TABLE [dbo].[Group]
 (
 	[Id] INT NOT NULL IDENTITY (1, 1) PRIMARY KEY, 
-    [Name] NVARCHAR(50) NULL
+    [Name] NVARCHAR(50) NULL, 
+    [SpecialozationId] INT NULL,
+    CONSTRAINT [FK_Group_Specialozation] FOREIGN KEY ([SpecialozationId]) REFERENCES [dbo].[Specialozation] ([Id]),
 )
