cfb80cd3ef777ef147bd5c42ee06a657e805403e
AlekseyGit-LegionY530 <chembrovich0303@mail.ru>
2021-08-31 12:07:36 +0300

    Created foreign key StudentId (faculty)

--- Faculty.Database/dbo/Tables/Faculty.sql
+++ Faculty.Database/dbo/Tables/Faculty.sql
@@ -2,5 +2,7 @@
 (
 	[Id] INT NOT NULL IDENTITY (1, 1) PRIMARY KEY, 
     [StartDateAducation] DATE NULL, 
-    [CountYearAducation] INT NULL
+    [CountYearAducation] INT NULL, 
+    [StudentId] INT NULL,
+    CONSTRAINT [FK_Faculty_Student] FOREIGN KEY ([StudentId]) REFERENCES [dbo].[Student] ([Id])
 )
