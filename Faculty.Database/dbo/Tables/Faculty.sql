3993d006e4ca5fa4fea035abd882505a6a943fe4
AlekseyGit-LegionY530 <chembrovich0303@mail.ru>
2021-08-31 12:05:25 +0300

    Created attribute CountYearAducation (faculty)

--- Faculty.Database/dbo/Tables/Faculty.sql
+++ Faculty.Database/dbo/Tables/Faculty.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE [dbo].[Faculty]
 (
 	[Id] INT NOT NULL IDENTITY (1, 1) PRIMARY KEY, 
-    [StartDateAducation] DATE NULL
+    [StartDateAducation] DATE NULL, 
+    [CountYearAducation] INT NULL
 )
