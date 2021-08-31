10db2be1263ee82823cf2e8d892e7a073c749b3f
AlekseyGit-LegionY530 <chembrovich0303@mail.ru>
2021-08-31 12:11:07 +0300

    Created foreign key CuratorId (faculty)

--- Faculty.Database/dbo/Tables/Faculty.sql
+++ Faculty.Database/dbo/Tables/Faculty.sql
@@ -5,6 +5,8 @@
     [CountYearAducation] INT NULL, 
     [StudentId] INT NULL,
     [GroupId] INT NULL, 
+    [CuratorId] INT NULL, 
     CONSTRAINT [FK_Faculty_Student] FOREIGN KEY ([StudentId]) REFERENCES [dbo].[Student] ([Id]),
-    CONSTRAINT [FK_Faculty_Group] FOREIGN KEY ([GroupId]) REFERENCES [dbo].[Group] ([Id])
+    CONSTRAINT [FK_Faculty_Group] FOREIGN KEY ([GroupId]) REFERENCES [dbo].[Group] ([Id]),
+    CONSTRAINT [FK_Faculty_Curator] FOREIGN KEY ([CuratorId]) REFERENCES [dbo].[Curator] ([Id])
 )
