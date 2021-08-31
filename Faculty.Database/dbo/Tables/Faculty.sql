f222136f16ff185856cc229e3df09b2c9d1f37f1
AlekseyGit-LegionY530 <chembrovich0303@mail.ru>
2021-08-31 12:09:09 +0300

    Created attribute GroupId (faculty)

--- Faculty.Database/dbo/Tables/Faculty.sql
+++ Faculty.Database/dbo/Tables/Faculty.sql
@@ -4,5 +4,7 @@
     [StartDateAducation] DATE NULL, 
     [CountYearAducation] INT NULL, 
     [StudentId] INT NULL,
-    CONSTRAINT [FK_Faculty_Student] FOREIGN KEY ([StudentId]) REFERENCES [dbo].[Student] ([Id])
+    [GroupId] INT NULL, 
+    CONSTRAINT [FK_Faculty_Student] FOREIGN KEY ([StudentId]) REFERENCES [dbo].[Student] ([Id]),
+    CONSTRAINT [FK_Faculty_Group] FOREIGN KEY ([GroupId]) REFERENCES [dbo].[Group] ([Id])
 )
