6802604b55e222928c69fd6681a86a0ce98ac19b
AlekseyGit-LegionY530 <chembrovich0303@mail.ru>
2021-08-31 11:52:55 +0300

    Created attribute doublename (curator)

--- Faculty.Database/dbo/Tables/Curator.sql
+++ Faculty.Database/dbo/Tables/Curator.sql
@@ -2,5 +2,6 @@
 (
 	[Id] INT NOT NULL IDENTITY (1, 1) PRIMARY KEY, 
     [Surname] NVARCHAR(50) NULL, 
-    [Name] NVARCHAR(50) NULL
+    [Name] NVARCHAR(50) NULL, 
+    [Doublename] NVARCHAR(50) NULL
 )
