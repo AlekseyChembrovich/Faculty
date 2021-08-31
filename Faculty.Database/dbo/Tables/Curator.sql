bdb18b0628d42de44529503825d84c77f6303564
AlekseyGit-LegionY530 <chembrovich0303@mail.ru>
2021-08-31 11:53:54 +0300

    Created attribute phone (curator)

--- Faculty.Database/dbo/Tables/Curator.sql
+++ Faculty.Database/dbo/Tables/Curator.sql
@@ -3,5 +3,6 @@
 	[Id] INT NOT NULL IDENTITY (1, 1) PRIMARY KEY, 
     [Surname] NVARCHAR(50) NULL, 
     [Name] NVARCHAR(50) NULL, 
-    [Doublename] NVARCHAR(50) NULL
+    [Doublename] NVARCHAR(50) NULL, 
+    [Phone] NVARCHAR(20) NULL
 )
