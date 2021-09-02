ea5c0ff034ecd78a24f41697bda7150a987a9d1d
AlekseyGit-LegionY530 <chembrovich0303@mail.ru>
2021-09-02 17:35:11 +0300

    Added additional annotations

--- Faculty.DataAccessLayer/IRepository.cs
+++ Faculty.DataAccessLayer/IRepository.cs
@@ -8,9 +8,28 @@ namespace Faculty.DataAccessLayer
     /// <typeparam name="T">Database model.</typeparam>
     public interface IRepository<T>
     {
+        /// <summary>
+        /// Method for adding data to the database.
+        /// </summary>
+        /// <param name="entity">Entity model.</param>
         void Insert(T entity);
+
+        /// <summary>
+        /// Method for changing data to the database.
+        /// </summary>
+        /// <param name="entity">Entity model.</param>
         void Update(T entity);
+
+        /// <summary>
+        /// Method for removing data to the database.
+        /// </summary>
+        /// <param name="entity">Entity model.</param>
         void Delete(T entity);
+
+        /// <summary>
+        /// Method for selecting data from a database.
+        /// </summary>
+        /// <returns>Lots of Entity objects returned.</returns>
         IEnumerable<T> GetAll();
     }
 }
