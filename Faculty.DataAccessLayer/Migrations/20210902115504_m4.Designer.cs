13dffb094c8a2267ccfaabcf71e2660ced847af9
AlekseyGit-LegionY530 <chembrovich0303@mail.ru>
2021-09-02 14:57:59 +0300

    Added select method tests

--- Faculty.DataAccessLayer/Migrations/20210902115504_m4.Designer.cs
+++ Faculty.DataAccessLayer/Migrations/20210902115504_m4.Designer.cs
@@ -4,14 +4,16 @@ using Faculty.DataAccessLayer.RepositoryEntityFramework;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Faculty.DataAccessLayer.Migrations
 {
     [DbContext(typeof(DatabaseContextEntityFramework))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20210902115504_m4")]
+    partial class m4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
