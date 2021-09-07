6e151f08082c2ac46e27ff2e4a98eef6c5a75e07
AlekseyGit-LegionY530 <chembrovich0303@mail.ru>
2021-09-07 17:24:57 +0300

    Changed constraints for foreign keys and added more values in post-deploy script

--- Faculty.Database/dbo/Scripts/Deploy.sql
+++ Faculty.Database/dbo/Scripts/Deploy.sql
@@ -5,25 +5,29 @@ GO
 INSERT INTO [dbo].[Students] ([dbo].[Students].[Surname], [dbo].[Students].[Name], [dbo].[Students].[Doublename])
 	VALUES ('test1', 'test1', 'test1'),
 		   ('test2', 'test2', 'test2'),
-		   ('test3', 'test3', 'test3')
+		   ('test3', 'test3', 'test3'),
+		   ('test4', 'test4', 'test4'),
+		   ('test5', 'test5', 'test5')
 
 GO
 INSERT INTO [dbo].[Curators] 
 	([dbo].[Curators].[Surname], [dbo].[Curators].[Name], [dbo].[Curators].[Doublename], [dbo].[Curators].[Phone]) 
 		VALUES ('test1', 'test1', 'test1', '+375-33-111-11-11'), 
 			   ('test2', 'test2', 'test2', '+375-33-222-22-22'),
-			   ('test3', 'test3', 'test3', '+375-33-333-33-33')
+			   ('test3', 'test3', 'test3', '+375-33-333-33-33'),
+			   ('test4', 'test4', 'test4', '+375-33-444-44-44'),
+			   ('test5', 'test5', 'test5', '+375-33-555-55-55')
 
 GO
 INSERT INTO [dbo].[Specializations] ([dbo].[Specializations].[Name])
-	VALUES ('test1'), ('test2'), ('test3')
+	VALUES ('test1'), ('test2'), ('test3'), ('test4'), ('test5')
 
 GO
 INSERT INTO [dbo].[Groups] ([dbo].[Groups].[Name], [dbo].[Groups].[SpecializationId]) 
-		VALUES ('test1', 1), ('test2', 2), ('test3', 3)
+		VALUES ('test1', 1), ('test2', 2), ('test3', 1), ('test3', 2), ('test3', 3)
 
 GO
 INSERT INTO [dbo].[Faculties] 
 	([dbo].[Faculties].[StartDateEducation], [dbo].[Faculties].[CountYearEducation],
 	 [dbo].[Faculties].[StudentId], [dbo].[Faculties].[GroupId], [dbo].[Faculties].[CuratorId]) 
-		VALUES ('2021-09-01', 5, 1, 1, 1), ('2021-09-01', 4, 2, 2, 2), ('2021-09-01', 5, 3, 3, 3)
+		VALUES ('2021-09-01', 5, 1, 1, 1), ('2021-09-01', 4, 2, 2, 2), ('2021-09-01', 4, 3, 3, 3), ('2021-09-01', 5, 4, 3, 3), ('2021-09-01', 5, 5, 2, 2)
