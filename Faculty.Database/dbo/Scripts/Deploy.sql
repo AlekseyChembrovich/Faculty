98e46acddb99b5ede79bed04ed2b4e770945a352
AlekseyGit-LegionY530 <chembrovich0303@mail.ru>
2021-09-07 06:54:15 +0300

    Changed post-deploy script for name of the tables

--- Faculty.Database/dbo/Scripts/Deploy.sql
+++ Faculty.Database/dbo/Scripts/Deploy.sql
@@ -1,29 +1,29 @@
 ﻿GO
-USE [PublishTask]
+USE [Repository]
 
 GO
-INSERT INTO [dbo].[Student] ([dbo].[Student].[Surname], [dbo].[Student].[Name], [dbo].[Student].[Doublename])
+INSERT INTO [dbo].[Students] ([dbo].[Students].[Surname], [dbo].[Students].[Name], [dbo].[Students].[Doublename])
 	VALUES ('test1', 'test1', 'test1'),
 		   ('test2', 'test2', 'test3'),
 		   ('test1', 'test2', 'test3')
 
 GO
-INSERT INTO [dbo].[Curator] 
-	([dbo].[Curator].[Surname], [dbo].[Curator].[Name], [dbo].[Curator].[Doublename], [dbo].[Curator].[Phone]) 
+INSERT INTO [dbo].[Curators] 
+	([dbo].[Curators].[Surname], [dbo].[Curators].[Name], [dbo].[Curators].[Doublename], [dbo].[Curators].[Phone]) 
 		VALUES ('test1', 'test2', 'test3', '+375-33-111-11-11'), 
 			   ('test1', 'test2', 'test3', '+375-33-222-22-22'),
 			   ('test1', 'test2', 'test3', '+375-33-333-33-33')
 
 GO
-INSERT INTO [dbo].[Specialozation] ([dbo].[Specialozation].[Name])
+INSERT INTO [dbo].[Specializations] ([dbo].[Specializations].[Name])
 	VALUES ('test1'), ('test2'), ('test3')
 
 GO
-INSERT INTO [dbo].[Group] ([dbo].[Group].[Name], [dbo].[Group].[SpecialozationId]) 
+INSERT INTO [dbo].[Groups] ([dbo].[Groups].[Name], [dbo].[Groups].[SpecializationId]) 
 		VALUES ('test1', 1), ('test2', 2), ('test3', 3)
 
 GO
-INSERT INTO [dbo].[Faculty] 
-	([dbo].[Faculty].[StartDateAducation], [dbo].[Faculty].[CountYearAducation],
-	 [dbo].[Faculty].[StudentId], [dbo].[Faculty].[GroupId], [dbo].[Faculty].[CuratorId]) 
+INSERT INTO [dbo].[Faculties] 
+	([dbo].[Faculties].[StartDateEducation], [dbo].[Faculties].[CountYearEducation],
+	 [dbo].[Faculties].[StudentId], [dbo].[Faculties].[GroupId], [dbo].[Faculties].[CuratorId]) 
 		VALUES ('2021-09-01', 5, 1, 1, 1), ('2021-09-01', 4, 2, 2, 2), ('2021-09-01', 5, 3, 3, 3)
