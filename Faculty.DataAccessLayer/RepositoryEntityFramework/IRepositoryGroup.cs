e0c26eabaaa9eb4eb6fe0fb51eebc5b3c96070c6
AlekseyGit-LegionY530 <chembrovich0303@mail.ru>
2021-09-23 14:06:45 +0300

    Create include foreign key operation

---
+++ Faculty.DataAccessLayer/RepositoryEntityFramework/IRepositoryGroup.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Faculty.DataAccessLayer.Models;
+
+namespace Faculty.DataAccessLayer.RepositoryEntityFramework
+{
+    public interface IRepositoryGroup : IRepository<Group>
+    {
+        /// <summary>
+        /// Method for selecting data from a database and include foreign key.
+        /// </summary>
+        /// <returns>Lots of Entity objects returned.</returns>
+        IEnumerable<Group> GetAllIncludeForeignKey();
+    }
+}
